c10f2d397cf72dba49f48cdd87d4f1a442512f68
RyoBamboo <itakedaka@gmail.com>
2015-11-24 13:07:23 +0900

    クラスター間の距離を返す関数を追加

--- js/argorithm.js
+++ js/argorithm.js
@@ -2,22 +2,22 @@
  * クラスター分析のアルゴリズムを扱うクラス
  */
  // var samples = [
- // 	[165, 54],
- // 	[164, 51],
- // 	[165, 58],
- // 	[162, 46],
- // 	[174, 54],
- // 	[167, 55],
- // 	[179, 53],
- // 	[159, 48],
- // 	[165, 50],
- // 	[165, 49],
- // 	[162, 48],
- // 	[168, 55],
- // 	[166, 56],
- // 	[163, 50],
- // 	[164, 53],
- // 	[166, 53],
+ //	[165, 54],
+ //	[164, 51],
+ //	[165, 58],
+ //	[162, 46],
+ //	[174, 54],
+ //	[167, 55],
+ //	[179, 53],
+ //	[159, 48],
+ //	[165, 50],
+ //	[165, 49],
+ //	[162, 48],
+ //	[168, 55],
+ //	[166, 56],
+ //	[163, 50],
+ //	[164, 53],
+ //	[166, 53],
  // ];
 
  var samples = [
@@ -32,15 +32,38 @@ var elementCnt = samples[0].length; // 要素数
 
 data = [];
 
-var samplesCnt = samples.length;
-for (var i = 0; i < samplesCnt; i++) {
+var sampleCnt = samples.length;
+
+// データ間の距離を計算して配列に格納
+for (var i = 0; i < sampleCnt; i++) {
 	data[i] = new Array();
-	for (var j = i + 1; j < samplesCnt; j++) {
-		data[i][j] = getDistance(i, j, elementCnt, samples);
+	for (var j = i + 1; j < sampleCnt; j++) {
+		data[i][j] = getDistanceOfData(i, j, elementCnt, samples);
 	}
 }
 
- console.log(data);
+for (var i = 0; i < sampleCnt; i++) {
+	for (var j = i + 1; j < sampleCnt; j++) {
+		data[j][i] = data[i][j];
+	}
+}
+
+// 最小距離のクラスターを探す
+var min;
+var ci;
+var cj;
+for (var i = 0; i < sampleCnt; i++) {
+	for (var j = i + 1; j < sampleCnt; j++) {
+		if (!min || data[i][j] < min) {
+			min = data[i][j];
+			ci = i;
+			cj = j;
+		}
+	}
+}
+
+// 実行
+ console.log(min);
 
 /*---------------------------------------------------------------
  * 2つのデータ間の距離を取得する関数
@@ -51,7 +74,7 @@ for (var i = 0; i < samplesCnt; i++) {
  *
  * @return distance int ２つのデータ間の
  *------------------------------------------------------------*/
-function getDistance(sampleNum, nextSampleNum, elementCnt, data) {
+function getDistanceOfData(sampleNum, nextSampleNum, elementCnt, data) {
 
 	var distance = 0;
 	var sum = 0;
@@ -64,6 +87,65 @@ function getDistance(sampleNum, nextSampleNum, elementCnt, data) {
 
 	return distance;
 }
+
+
+/*---------------------------------------------------------------
+ * 2つのデータ間の距離を取得する関数
+ * @param method : 分析方法
+ * @param dio        : クラスターi, oの距離
+ * @param djo        : クラスターj, oの距離
+ * @param dij         : クラスターi, jの距離
+ * @param ni : クラスターiに含まれるデータ数
+ * @param nj : クラスターiに含まれるデータ数
+ * @param no : クラスターiに含まれるデータ数
+ * @param x ,y    : データ
+ *
+ * @return distance クラスター間の距離
+ *------------------------------------------------------------*/
+function getDistanceOfCluster(method, dio, djo, dij, ni, nj, no) {
+	distance = 0;
+
+	switch (method) {
+		// 最短距離法
+		case 1:
+			distance = Math.min(dio, djo);
+			break;
+	}
+
+	return distance;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var result = getCombination(samples, 2);
 /**
